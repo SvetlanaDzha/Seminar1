f27fda0542f48f4909f0dcd3310dce6e338b6a45
Svetlana <svetlanka2503@mail.ru>
2022-08-06 20:54:44 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.Write("Введите случайное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int counter = -N;
+while (counter <= N)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
